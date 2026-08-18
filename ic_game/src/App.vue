105dca9757cdfbbbd83aa3b365a58804ada4fea5
slimemat <72225068+slimemat@users.noreply.github.com>
2026-08-18 18:27:51 -0300

    Revert "(feature) Muda um pouco a estrutura do projeto para suportar múltiplo…"

--- ic_game/src/App.vue
+++ ic_game/src/App.vue
@@ -1,38 +1,7 @@
 <script setup>
-import { ref } from "vue";
-import MainMenu from "./components/MainMenu.vue";
-import PlaceholderGame from "./components/PlaceholderGame.vue";
-import TestGame from "./components/TestGame.vue";
-
-const games = [
-  {
-    id: "test-platformer",
-    number: "01",
-    title: "Star Runner",
-    description: "Teste do phaser com plataforma e coleta de itens.",
-    status: "Play",
-  },
-  {
-    id: "logic-lab",
-    number: "02",
-    title: "Logic Lab",
-    description: "Um novo jogo de resolução de problemas.",
-    status: "Soon",
-  },
-];
-
-const activeGame = ref("menu");
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <MainMenu
-    v-if="activeGame === 'menu'"
-    :games="games"
-    @select="activeGame = $event"
-  />
-  <TestGame
-    v-else-if="activeGame === 'test-platformer'"
-    @back="activeGame = 'menu'"
-  />
-  <PlaceholderGame v-else @back="activeGame = 'menu'" />
+  <HelloWorld />
 </template>
